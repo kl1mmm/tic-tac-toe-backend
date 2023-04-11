7dab23e4064f658eb3b03aead1988ce255e735e2
kl1mmm <klimovilya07@mail.ru>
2023-04-11 14:43:22 +0300

    user model migrate

--- tictactoe_back/users/models.py
+++ tictactoe_back/users/models.py
@@ -5,8 +5,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
+    user_sex = [
+        ('М', 'Мужской'),
+        ('Ж', 'Женский'),
+    ]
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    patronymic_name = models.CharField(("patronymic"), max_length=150, blank=True)
-    birth_date = models.DateField(("birthday"), null=False, blank=False)
-    sex = models.CharField(("user sex"), max_length=1, blank=False)
-    edited_date = models.DateTimeField(("date edited"), default=timezone.now, null=False, blank=False)
+    patronymic_name = models.CharField(("Отчество"), max_length=150, blank=True)
+    birth_date = models.DateField(("Дата рождения"), null=False, blank=False)
+    sex = models.CharField(("Пол"), max_length=1, choices=user_sex, blank=False)
+    edited_date = models.DateTimeField(("Дата изменения профиля"), default=timezone.now, null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Профиль',
+        verbose_name_plural = 'Профили'

02d5bef66757746337b763f82a7310779b70427e
kl1mmm <klimovilya07@mail.ru>
2023-04-11 15:41:09 +0300

    PlayerStat init and works

---
+++ tictactoe_back/stats/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class PlayerStatistic(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    count_of_games = models.IntegerField(('Кол-во игр'), null=False, blank=False)
+    count_of_wins = models.IntegerField(('Побед'), null=False, blank=False)
+    count_of_loses = models.IntegerField(('Поражений'), null=False, blank=False)
+
+    def percent_of_wins(self):
+        return (round(self.count_of_wins / self.count_of_loses)) * 100
+
+    class Meta:
+        verbose_name = 'Статистика',
+        verbose_name_plural = 'Статистика'

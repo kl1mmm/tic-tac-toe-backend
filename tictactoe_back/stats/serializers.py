4abdee2d7261978451dffccc40f1134f20cc3f3b
kl1mmm <klimovilya07@mail.ru>
2023-04-19 19:12:04 +0300

    serializers upd

--- tictactoe_back/stats/serializers.py
+++ tictactoe_back/stats/serializers.py
@@ -4,6 +4,6 @@ from .models import PlayerStatistic
 
 class StatsSerializer(serializers.Serializer):
     user = serializers.CharField()
-    count_of_games = serializers.IntegerField()
+    count_of_games = serializers.IntegerField(read_only=True)
     count_of_wins = serializers.IntegerField(read_only=True)
     count_of_loses = serializers.IntegerField(read_only=True)
--- tictactoe_back/users/serializers.py
+++ tictactoe_back/users/serializers.py
@@ -15,8 +15,8 @@ class ProfileSerializer(serializers.Serializer):
 
 
 class UserSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
+    id = serializers.IntegerField(read_only=True)
     username = serializers.CharField(max_length=150)
     first_name = serializers.CharField(max_length=150)
     last_name = serializers.CharField(max_length=150)
-    details = ProfileSerializer(source='profile')
+    details = ProfileSerializer(source='profile', required=False)
